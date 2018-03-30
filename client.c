13e4cabea576746eedaf69c55d161eb56782b57a
Henchhing Limbu <henchhinglimbu@gmail.com>
2018-03-30 05:24:16 -0400

    updated server.c and client.c

--- client.c
+++ client.c
@@ -4,69 +4,70 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // Global constants
-#define ECHO_PORT (5000)
 #define MAX_LINE (1025)
 
+
 int main() {
-	int list_socket;						// listening socket
+	int conn_socket;						// listening socket
 	int bind_socket;						// binding socket
-	int connection_socket; 					// connections socket
 	short int port;							// port number
 	// socket address structures
 	// particular for of the sockaddr used for TCP/IP addresses
 	struct sockaddr_in servaddr;
 	char buffer[MAX_LINE];					// character buffer
-	char *endptr;							// for strtol()
 	int filesize;							// size of the file to send to server
 	FILE* file_to_send;						// file to send
+	char* ip_address;						// holds remote IP address
+	char* port_num;							// holds remote port number
+	char* endptr;							// for strtol()
+
+	// setting the remote port
+	port = strtol(port_num, &endptr, 0);
+	if (*endptr) {
+		printf("ECHOCLIENT: Invalid port supplied.\n");
+		exit(EXIT_FAILURE);
+	}
 	
 	// creating the listening socket
-	list_socket = socket(AF_INET, SOCK_STREAM, 0);
+	conn_socket = socket(AF_INET, SOCK_STREAM, 0);
 	
 	// checking for the error
-	if (list_socket < 0) {
-		printf("ECHOSERV: Error creating listening socket.\n");
+	if (conn_socket < 0) {
+		printf("ECHOCLIENT: Error creating listening socket.\n");
 		exit(EXIT_FAILURE);
 	}
 
 	// set all the bytes is socket structure to 0
-	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&servaddr, '0', sizeof(servaddr));
 	
 	// filling in the relative data members 
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(port);
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	// setting the remote IP address
+	if ( inet_aton(ip_address, &servaddr.sin_addr ) < 0 ) {
+		printf("ECHOCLIENT: Invalid remote IP address.\n");
+		exit(EXIT_FAILURE);
+	}
 
 	// bind our socket address to the listening socket, and call listen()
-	bind_socket = bind(list_socket, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	
-	// checking if the socket is binded properly
-	if (bind_socket < 0) {
-		printf("ECHOSERV: Error calling bind()\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	// instructs TCP protocol implementation to listen for connections
-	if (listen(list_socket, LISTENQ) < 0) {
-		printf("ECHOSERV: Error calling listen()\n");
-		exit(EXIT_FAILURE);
-	}
+	bind_socket = bind(conn_socket, (struct sockaddr *) &servaddr, sizeof(servaddr));
 	
 	printf("TCP Client establishing connection...");
 	
 	// establishing connection to the server 
-	connection_socket = connect(list_socket, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	
 	// checking for error
-	if (connection_socket < 0) {
+	if (connect(conn_socket, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
 		printf("ECHOSERV: Error establishing connection.\n");
 		exit(EXIT_FAILURE);
 	}
 	
 	// TODO: need change here 
-	file_to_send = fopen("practic_project_test_file_1");
+	file_to_send = fopen("practic_project_test_file_1","rb");
 	// moving the pointer to the end of the file
 	fseek(file_to_send, 0, SEEK_END);
 	// finding the size of the file
@@ -75,37 +76,19 @@ int main() {
 	// resetting the position indicator to the original state
 	rewind(file_to_send);
 	
-	// read data from to buffer
-	fread(buffer,1,filsize, file_to_send);
+	// read data file to buffer
+	fread(buffer,1,filesize, file_to_send);
 	
 	// writing the data of the file to the socket
-	if (write(list_socket, buffer, filesize) < 0) {
-		printf("ECHOSERV: Error writing to the socket.\n");
+	// if error in writing show error message
+	if (write(conn_socket, buffer, filesize) < 0) {
+		printf("ECHOCLIENT: Error writing to the socket.\n");
 	}
-	if (read(list_socket, buffer, filesize) < 0) 
-		printf("ECHOSERV: Error reading from the socket.\n");
+	// if error in reading show error message
+	if (read(conn_socket, buffer, filesize) < 0) 
+		printf("ECHOCLIENT: Error reading from the socket.\n");
 	
-	
-	
-	/*
-	// enter an infinte loop to respond to client requests
-	while (1) {
-		connection_socket = accept(list_socket, NULL, NULL);
-		if ( connection_socket ) < 0) {
-			printf("ECHOSERV: Error calling accpet().\n");
-			exit(EXIT_FAILURE);
-		}
-		Readline(connection_socket, buffer, MAX_LINE - 1);
-		Writeline(connection_socket, buffer, strlen(buffer));
-	
-		// close the connect socket
-		if (close(connection_socket) < 0) {
-			fprintf("ECHOSERV: Error calling close()\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-	*/
-	return 0;
+	return EXIT_SUCCESS;
 }
 
 // get 
--- server.c
+++ server.c
@@ -1,42 +1,72 @@
+
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define ECHO_PORT (2002)
+#define MAX_LINE (1025)
+#define LISTENQ (1024)
 
 int main() {
-	// server socket
-	int server_socket;
+	// listening socket
+	int list_socket;
 	
-	// creating server socket
-	server_socket = socket(AF_NET, SOCK_STREAM, 0);
+	// socket address structure
+	struct sockaddr_in serv_addr;
 	
-	// checking for erro
-	if (server_socket < 0) {
+	char buffer[MAX_LINE];
+	// TODO: Change needed here
+	short int port = ECHO_PORT;
+	
+	// creating listening socket
+	list_socket = socket(AF_NET, SOCK_STREAM, 0);
+	
+	// checking for error
+	if (list_socket < 0) {
 		printf("ECHOSERV: Error creating a server socket.\n");
 		exit(EXIT_FAILURE);
 	}
 	
-	// socket address
-	struct sockaddr_in serv_addr;
-	
 	// initializing every member of socket_addr with 0
-	memset(&serv_addr, 0, sizeof(serv_addr);
+	memset(&serv_addr, '0', sizeof(serv_addr));
 	
 	serv_addr.sin_family = AF_NET;
-	serv_addr.sin_port = htons(PORT);
+	serv_addr.sin_port = htons(port);
+	// assigning a random address
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	
-	list_s = socket(AF_NET, SOCK_STREAM, 0);	if (list_s < 0) {
-		printf("ECHOSERV: Error creating listening socket.\n");
+	// binding the socket address to the listening socket
+	if ( bind(list_socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0 ) {
+		printf("ECHOSERV: Error calling bind().\n");
+		exit(EXIT_FAILURE);
+	}
+	// listening to the socket
+	if (listen(list_socket, LISTENQ) < 0) {
+		printf("ECHOSERV: Error listening to the socket.\n");
 		exit(EXIT_FAILURE);
 	}
 	
-	if (connect(server_socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		printf("ECHOSERV: Error connecting socket.\n");
-		exit(EXIT_FAILURE);
+	int connection_socket;
+	// enter an infinte loop to respond to client requests
+	while (1) {
+		// accepting a connection
+		connection_socket = accept(list_socket, NULL, NULL);
+		if ( connection_socket  < 0) {
+			printf("ECHOSERV: Error calling accpet().\n");
+			exit(EXIT_FAILURE);
+		}
+		// Readline(connection_socket, buffer, MAX_LINE - 1);
+		// Writeline(connection_socket, buffer, strlen(buffer));
+	
+		// close the connect socket
+		if (close(connection_socket) < 0) {
+			printf("ECHOSERV: Error calling close()\n");
+			exit(EXIT_FAILURE);
+		}
 	}
-	
-	
 	return 0;
 }
